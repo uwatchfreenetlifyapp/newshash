9229ddfb27a3649b3e9be3659175fdbe08316b67
groupincworld <114858599+uwatchfreenetlifyapp@users.noreply.github.com>
2023-02-14 03:05:20 +0530

    up

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://hbomaxmovies.netlify.app/',
-    changefreq: 'daily',
-    priority: 0.80,
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-  }
