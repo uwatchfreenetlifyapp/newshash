221d6c23b2356e9795e99a12036f4063a15b6003
groupincworld <114858599+uwatchfreenetlifyapp@users.noreply.github.com>
2023-02-14 03:06:40 +0530

    up

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mxplayermovies.netlify.app/',
+    changefreq: 'daily',
+    priority: 0.80,
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
