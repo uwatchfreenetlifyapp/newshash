18251c24f98a66395e9aa6ad6c73847c8f685a93
groupincworld <114858599+uwatchfreenetlifyapp@users.noreply.github.com>
2023-03-11 16:23:30 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     siteUrl: 'https://uwatch4free.netlify.app/',
     changefreq: 'daily',
-    priority: 0.80,
+    priority: 0.90,
     generateRobotsTxt: true,
     sitemapSize: 7000,
   }
